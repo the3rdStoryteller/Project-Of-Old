dfef60f57a18a1f46bf812b628e9ab299fb28688
Ethan <sowletha@gmail.com>
2024-02-17 00:24:46 -0500

    added Project

---
+++ Assets/Door.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    
+    void Start() {
+        transform.position = new Vector3(0, 0, 0);
+    }
+}
