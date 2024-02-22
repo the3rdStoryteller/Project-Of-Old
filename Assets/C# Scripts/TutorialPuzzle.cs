18abcc7af7ecb0047386f5c48e304cfd074e68ff
Ethan Sowle <sowletha@gmail.com>
2024-02-22 18:40:53 -0500

    fixed bug

--- Assets/C# Scripts/TutorialPuzzle.cs
+++ Assets/C# Scripts/TutorialPuzzle.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class TutorialPuzzle : MonoBehaviour
