eaf65dd07dcb35b5206f87b3ee224b0c5b930dec
Ethan Sowle <sowletha@gmail.com>
2024-02-22 18:38:13 -0500

    added door triggers

---
+++ Assets/C# Scripts/FloorTriggers.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTriggers : MonoBehaviour
+{
+    bool isNotTriggered = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This method is called when another collider enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider belongs to the player
+        if (other.gameObject.CompareTag("Player") && isNotTriggered)
+        {
+            // The player has collided with the trigger
+            Debug.Log("Player has collided with the trigger");
+            isNotTriggered = false;
+            TutorialPuzzle.addToActiveTriggers(this);
+        }
+    }
+}
