b4b091eeecdf94ae6cb7a689dbee05ec4f5d70fb
Ethan <sowletha@gmail.com>
2024-03-29 01:32:52 -0400

    added textures

--- Assets/C# Scripts/Door.cs
+++ Assets/C# Scripts/Door.cs
@@ -22,7 +22,7 @@ public class Door : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
         trans = GetComponent<Transform> ();
     }
