8babdd334e79737f25eaae5d823387d4271e8aed
Ethan <sowletha@gmail.com>
2024-04-11 19:43:24 -0400

    tweaked jump mechanic and revamped EnemyAI

---
+++ Assets/C# Scripts/EnemyAI.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* EnemyAI.cs
+*   Author: Ethan Sowle
+*   Description: A class that controls the enemy's AI behavior.
+*   Parametes: None
+*   Return: None
+*   Date Created: 4/11/2024
+*   Date Modified: 4/11/2024
+*/
+
+public class EnemyAI : MonoBehaviour
+{
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+    public Transform target;
+    public GameObject playerOBJ;
+    public float pathUpdateDelay = 0.2f;
+    private float pathUpdateDeadline;
+    public int maxHealth = 5;
+    private int currentHealth;
+    public int attackDMG = 2;
+    private float attackRange;
+    public float attackDelay = 1.0f;
+    private bool isAttacking = false;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackRange = navMeshAgent.stoppingDistance;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            bool inRange = Vector3.Distance(transform.position, target.position) <= attackRange;
+
+            if (inRange)
+            {
+                LookAtTarget();
+            }
+            else
+            {
+                UpdatePath();
+            }
+        }
+
+        // Check if the player is within attack range
+        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+
+        // Attack the player if they are within range
+        if (distanceToPlayer <= attackRange)
+        {
+            Attack();
+        }
+    }
+
+    // Function to look at the target
+    private void LookAtTarget()
+    {
+        Vector3 lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+    }
+
+    // Function to track target
+    private void UpdatePath()
+    {
+        if (Time.time >= pathUpdateDeadline)
+        {
+            //Debug.Log("Updating path");
+            pathUpdateDeadline = Time.time + pathUpdateDelay;
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+
+    // Function to attack the player
+    public void Attack()
+    {
+        // Only attack if not already attacking
+        if(!isAttacking)
+        {
+            // Set attacking bool to true
+            isAttacking = true;
+
+            // Get the player's health script
+            Player playerHealth = playerOBJ.GetComponent<Player>();
+
+            // Reduce the player's health
+            playerHealth.TakeDamage(attackDMG);
+
+            // Start attack delay coroutine
+            StartCoroutine(AttackDelay());
+        }
+    }
+
+    // Coroutine for attack delay
+    IEnumerator AttackDelay()
+    {
+        // Wait for attack delay and reset attacking bool
+        yield return new WaitForSeconds(attackDelay);
+        isAttacking = false;
+    }
+
+    // Function to take damage
+    public void TakeDamage(int damage)
+    {
+        // Reduce the current health by the damage amount.
+        currentHealth -= damage;
+
+        Debug.Log("Enemy took " + damage + " damage.");
+        Debug.Log("Current health: " + currentHealth);
+
+        // Check if the enemy is dead.
+        if (currentHealth <= 0)
+        {
+            // Call the Die function.
+            Die();
+        }
+    }
+
+    // Function for death
+    void Die()
+    {
+        // Destroy the enemy object
+        Destroy(this.gameObject);
+    }
+}
--- Assets/C# Scripts/Player.cs
+++ Assets/C# Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 /* Player.cs
@@ -6,15 +7,20 @@ using UnityEngine;
 *   Parametes: None
 *   Return: None
 *   Date Created: 2/6/2024
-*   Date Modified: 4/10/2024
+*   Date Modified: 4/11/2024
 */
 
 public class Player : MonoBehaviour {
     public int health = 10;
     public int attackDMG = 3;
     public float attackRange = 1.0f;
+    public AudioClip attackSound;
+    public AudioClip deathSound;
+    public AudioSource audioSource;
     public float speed = 3.0F;
-    public float jumpSpeed = 4.0F;
+    public float jumpSpeed = 0.5F;
+    public float jumpDuration = 0.2f; // The maximum time the player can jump
+    private float jumpTimer;
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
     public LayerMask enemyLayers;
@@ -24,27 +30,45 @@ public class Player : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
+        // Get the CharacterController and AudioSource components
         controller = GetComponent<CharacterController>();
+        audioSource = GetComponent<AudioSource>();
+
+        // Set the respawn point to the player's starting position
         respawnPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the player
-        if (controller.isGrounded) // Check if the player is on the ground
+        // Moves the player
+        // Check if the player is on the ground
+        if (controller.isGrounded) 
         {
             moveDirection = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
 
-            if (Input.GetButton("Jump")) // Check if the Jump button (spacebar by default) is pressed
+            // Check if the Jump button (spacebar by default) is pressed
+            if (Input.GetButton("Jump"))
             {
-                moveDirection.y = jumpSpeed; // Apply an upward force
+                // Start the jump timer
+                jumpTimer = jumpDuration;
+                moveDirection.y = jumpSpeed;
             }
         }
-
-        moveDirection.y += Physics.gravity.y * Time.deltaTime; // Apply gravity
+        // Check if the player is in the middle of a jump
+        else if (jumpTimer > 0)
+        {
+            // Continue the jump
+            jumpTimer -= Time.deltaTime;
+            moveDirection.y += Physics.gravity.y * Time.deltaTime;
+        }
+        
+        // Apply gravity
+        moveDirection.y += Physics.gravity.y * Time.deltaTime;
+        
+        // Apply movement
         controller.Move(moveDirection * Time.deltaTime);
 
         // Attack
@@ -53,13 +77,7 @@ public class Player : MonoBehaviour {
             Attack();
         }
 
-        // Respawns Player if killed
-        if(health <= 0)
-        {
-            Respawn();
-        }
-
-        // Respawn Player if they fall off the map
+        // Respawn the player if they fall off the map
         if (transform.position.y < respawnYPos)
         {
             Respawn();
@@ -71,25 +89,75 @@ public class Player : MonoBehaviour {
     {
         // Detect enemies in range of attack
         Collider[] hitEnemies = Physics.OverlapSphere(transform.position, attackRange, enemyLayers);
-       
-        /*Debug.Log(transform.position);
-        Debug.Log(attackRange);
-        Debug.Log(enemyLayers);*/
 
         // Damage them
         foreach (Collider enemy in hitEnemies)
         {
-            if (enemy.TryGetComponent<EnemyControl>(out var enemyControl))
+            EnemyAI enemyAI = enemy.GetComponent<EnemyAI>();
+            if (enemyAI != null)
             {
-                enemyControl.TakeDamage(attackDMG);
+                enemyAI.TakeDamage(attackDMG);
             }
         }
+
+        // Play attack sound
+        audioSource.PlayOneShot(attackSound);
     }
+
+    // Function to handle player taking damage
+    public void TakeDamage(int damage)
+    {
+        // Reduce the health by the damage amount.
+        health -= damage;
+
+        Debug.Log("Player took " + damage + " damage.");
+        Debug.Log("Current health: " + health);
+
+        // Check if the player is dead.
+        if (health <= 0)
+        {
+            // Call the Die function.
+            Die();
+        }
+    }
+
+    // Function to handle player death
+    void Die()
+    {
+        // Play death sound
+        audioSource.PlayOneShot(deathSound);
+
+        // Start the DisableAfterDelay coroutine, passing in the length of the death sound
+        //StartCoroutine(DisableAfterDelay(deathSound.length));
+
+        // Disable the Player object
+        this.gameObject.SetActive(false);
+
+        // Call Respawn function
+        Respawn();
+    }
+
+    // Coroutine to disable the GameObject after a delay
+    /*IEnumerator DisableAfterDelay(float delay)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        // Disable the Player object
+        this.gameObject.SetActive(false);
+
+        // Call Respawn function
+        Respawn();
+    }*/
 
     // Function to handle player respawning
     void Respawn() 
     {
+        // Reset player's health and position
         health = 10;
         transform.position = respawnPoint;
+
+        // Re-enable the Player object
+        this.gameObject.SetActive(true);
     }
 }
